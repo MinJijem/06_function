10d8c44de91dc6a659f40a57063ba0c59b8ec181
MinJijem <55428081+MinJijem@users.noreply.github.com>
2019-10-15 15:39:38 +0900

    practice6

--- main.c
+++ main.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h> 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-//practice 4
-	int square(int n);
+//practice 6
+	
 
 int main(int argc, char *argv[]) {
 	
-	int result;
-	result=square(5);
-	printf("%d",result);
+	
+	int i;
+	int max=45;
+	
+	srand((unsigned) time(NULL));//랜덤한 패턴으로 나오게 해준다  
+	for(i=0;i<6;i++)
+	printf("%d ",1+rand()%max);
+	return 0;
 	
 }
 
-	int square(int n)
-	{
-		return(n*n);
-	}
-
+	
 
 
 
