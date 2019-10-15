67e7a63a954029340363fe6fdfcf9cc767d31f61
MinJijem <55428081+MinJijem@users.noreply.github.com>
2019-10-15 15:21:35 +0900

    06_practice4

--- main.c
+++ main.c
@@ -3,43 +3,22 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-//practice 2
-
-int sumTwo(int a,int b)
-{
-	int output;
-	output=a+b;
-	return output;
-}
-
-int square( int n)
-{
-	return(n*n);
-}
-
-int get_max(int a, int b)
-{
-	if(a>b) return(a);
-	else return(b);
-}
+//practice 4
+	int square(int n);
 
 int main(int argc, char *argv[]) {
 	
-	//practice3
-
-	int x,y;
-	int output;
+	int result;
+	result=square(5);
+	printf("%d",result);
 	
-	x=2;
-	y=5;
-	
-	output = sumTwo(x,y);
-	printf("sumtwo : %i\n",output);
-	
-	printf("square : %i\n",square(x));
-	
-	output = get_max(x,y);
-	printf("max : %i\n",output);
-
-	return 0;
 }
+
+	int square(int n)
+	{
+		return(n*n);
+	}
+
+
+
+
